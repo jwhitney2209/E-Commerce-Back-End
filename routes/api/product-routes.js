396effc2f3e54f1b3ae26993451a220fa1ee6d44
teachergg <jwhitney@bisd.us>
2022-05-06 22:25:39 -0500

    finish models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
   // be sure to include its associated Category and Tag data
 });
 
